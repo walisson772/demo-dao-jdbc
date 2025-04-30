ca210b0b368cda5756c231c1203605a3ba4288e1
Walisson Santos <walissonsantost008@gmail.com>
2025-04-30 19:25:21 -0300

    SellerDaoJDBC

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,29 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
+}
