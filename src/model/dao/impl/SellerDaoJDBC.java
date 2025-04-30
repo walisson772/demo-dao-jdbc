560c74f85a4b9e09bf8030e3338702bfe9e7f73c
Walisson Santos <walissonsantost008@gmail.com>
2025-04-30 20:17:12 -0300

    Implementando insert

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -6,10 +6,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,7 +22,39 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void insert(Seller obj) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "INSERT INTO seller \n" +
+                            "(Name, Email, BirthDate, BaseSalary, DepartmentId)  \n" +
+                            "VALUES  \n" +
+                            "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
 
+            int linhasAfetadas = st.executeUpdate();
+
+            if (linhasAfetadas > 0){
+                ResultSet sr = st.getGeneratedKeys();
+                if (sr.next()){
+                    int id = sr.getInt(1);
+                }
+                DB.closeResultSet(sr);
+            }
+            else {
+                throw new DbException("Error inerperado");
+            }
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
