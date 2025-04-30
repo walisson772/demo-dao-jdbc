f281546e39f616b7fbeb0f802e6f96058075cc40
Walisson Santos <walissonsantost008@gmail.com>
2025-04-30 18:58:05 -0300

    Criando um projeto no

--- src/db/DB.java
+++ src/db/DB.java
@@ -14,6 +14,7 @@ public class DB {
 	private static Connection conn = null;
 	
 	public static Connection getConnection() {
+		System.out.println(".");
 		if (conn == null) {
 			try {
 				Properties props = loadProperties();
