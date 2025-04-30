a8f70cb28ab2884fae5d65842a1104b279e499b5
Walisson Santos <walissonsantost008@gmail.com>
2025-04-30 20:06:10 -0300

    Melhorando o codigo

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,6 @@
 package model.dao;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.List;
@@ -9,5 +10,6 @@ public interface SellerDao {
     void update(Seller obj);
     Seller findById(Integer id);
     List<Seller> findAll();
+    List<Seller> findByDepartment(Department obj);
 
 }
